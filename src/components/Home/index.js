b751b87cdcbb77a163e7b1de92e40ace3a3d3d87
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-04-24 11:28:43 -0400

    create home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Home() {
+    return (
+        <section className="">
+            
+            <h1 id="about"></h1>
+            <h1 id="about"></h1>
+            <a> Enter</a>
+        </section>
+    );
+}
+
+export default Home;
